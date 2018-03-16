29db11c8580e485b2b8c1080cd21ae69c5e9765b
Danilo <dakarasadnik@gmail.com>
2018-03-16 13:22:01 +0100

    Language middleware added

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class Language
 {
@@ -13,8 +14,21 @@ class Language
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
+        // Check if the first segment matches a language code
+        if(!array_key_exists($request->segment(1),config('translatable.locales'))){
+
+            // Store segments in array
+            $segments = $request->segments();
+
+            // Set the default language code as the first segment
+            $segments = array_prepend($segments,config('app.fallback_locale'));
+
+            // Redirect to the correct url
+            return redirect()->to(implode('/',$segments));
+
+        }
         return $next($request);
     }
 }
